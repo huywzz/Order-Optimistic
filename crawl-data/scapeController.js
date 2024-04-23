8594b112c5d9b4d0f6a05188a68c9b81e2f2545f
HuyHue <dqh1005@gmail.com>
2024-04-23 15:42:08 +0700

    crawl-data:get link product

--- crawl-data/scapeController.js
+++ crawl-data/scapeController.js
@@ -3,10 +3,15 @@ const scaper = require("./scaper");
 
 const scapeController = async (browserInstance) => {
     const url = 'https://fptshop.com.vn/'
+    const indexs=[0,1,2,4]
     try {
         let browser = await browserInstance
 
-        let category = scaper.scapeCategory(url, browser)
+        let categories = await scaper.scapeCategory(url, browser)
+        const selectedCate = categories.filter((category,index) =>  indexs.some(i=>i===index) )
+        const listProduct = await scaper.scaperEachLink(browser, selectedCate[0].link)
+        console.log('>>list Product:',listProduct);
+        
 
     } catch (error) {
         console.log('Loi o scapeController ', error);
--- crawl-data/scaper.js
+++ crawl-data/scaper.js
@@ -21,15 +21,47 @@ const scapeCategory = async (url, browser) => new Promise(async (resolve, reject
             return dataCategory
         })
         console.log(dataCategory);
-
         await page.close()
         console.log('>>tab đã đóng');
-        resolve()
+        resolve(dataCategory)
+
     } catch (error) {
         console.log('loi o scapeCategory', error);
         reject(error)
     }
 })
+const scaperEachLink = (browserIntance,url) => new Promise(async(resolve, reject) => {
+    try {
+        let newPage = await browserIntance.newPage()
+        console.log('>>Mở tab mới ');
+        await newPage.goto(url, { timeout: 120000 })
+        console.log('>>Truy cập vào ', url);
+        await newPage.waitForSelector('#root > main > div > div.row.fspdbox > div.col-9.p-0 > div.card.fplistbox > div > div.cdt-product-wrapper.m-b-20')
+        
+        console.log('>>Load web đã xong')
+        
+        const dataProduct=await newPage.$$eval('div > div.cdt-product__info > h3'
+            , els => {
+                dataProduct = els.map(el => {
+                    console.log(el);
+                    return {
+                        nameProduct: el.querySelector('a').title,
+                        link: `https://fptshop.com.vn${el.querySelector('a').getAttribute('href')}`,
+                    }
+                }) 
+                return dataProduct
+            })
+                                    
+
+        await browserIntance.close()
+        resolve(dataProduct)
+
+    } catch (error) {
+        console.log('loi o spacer product', error);
+        reject(error)
+    }
+})
 module.exports = {
-    scapeCategory
+    scapeCategory,
+    scaperEachLink
 }
