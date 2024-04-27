aa8a4cdbf15b898a936d79d01c871bc87acc076d
HuyHue <dqh1005@gmail.com>
2024-04-27 22:38:47 +0700

    database: customer.model, order.model

---
+++ src/models/customer.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose')
+
+const COLLECTION_NAME = 'Customers'
+const DOCUMENT_NAME = 'Customer'
+
+const customerSchema = new Schema({
+    phone_customer:{types:String,require:true},
+    email_customer:{types:String, require:true},
+    name_customer: { types: String, require: true },
+}, {
+    timeseries: true,
+    collection: COLLECTION_NAME,
+}
+)
+module.exports = {
+    customerModel:model(DOCUMENT_NAME,customerSchema)
+}
+
---
+++ src/models/order.model.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose')
+
+const COLLECTION_NAME = 'Orders'
+const DOCUMENT_NAME = 'Order'
+
+const orderSchema = new Schema({
+    order_customer: { types: Schema.Types.ObjectId, ref: 'customer' },
+    order_total: { types: Number, require: true },
+    order_products: {
+        type: Array,
+        default: []
+    }
+    
+}, {
+    timestamps: true,
+    collection:COLLECTION_NAME
+})
+
+module.exports = model(DOCUMENT_NAME, orderSchema)
+//order_detai
+// {
+//     product_id: Schema.Types.ObjectId,
+//     product_name:String,
+//     product_price:String,
+//     quantity:Number,
+//     total:Number,
+// }
+
