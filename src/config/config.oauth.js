0283e20692a0669f43747e4d5243aab77d7190da
HuyHue <dqh1005@gmail.com>
2024-05-02 17:09:37 +0700

    config oauth

---
+++ src/config/config.oauth.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    CLIENT_ID:process.env.CLIENT_ID,
+    CLIENT_SECRET:process.env.CLIENT_SECRET,
+    REDIRECT_URI:process.env.REDIRECT_URI,
+    REFRESH_TOKEN:process.env.REFRESH_TOKEN
+}
